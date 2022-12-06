c80a347180b4f888596f926a65cf301a0101bb57
Tom LeGodais <tomlegodais@gmail.com>
2022-12-06 11:38:29 -0500

    Remove ignored files

---
+++ target/debug/day01.d
@@ -0,0 +1,1 @@
+C:\Users\Tom\ LeGodais\IdeaProjects\aoc-2022\target\debug\day01.exe: C:\Users\Tom\ LeGodais\IdeaProjects\aoc-2022\src\bin\day01\main.rs C:\Users\Tom\ LeGodais\IdeaProjects\aoc-2022\src\lib.rs
---
+++ target/debug/day02.d
@@ -0,0 +1,1 @@
+C:\Users\Tom\ LeGodais\IdeaProjects\aoc-2022\target\debug\day02.exe: C:\Users\Tom\ LeGodais\IdeaProjects\aoc-2022\src\bin\day02\main.rs C:\Users\Tom\ LeGodais\IdeaProjects\aoc-2022\src\lib.rs
