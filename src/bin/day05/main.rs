ed889bff953bd3ec8177ff1785cb8e7cb9d5b0c4
Tom LeGodais <tomlegodais@gmail.com>
2022-12-08 12:38:53 -0500

    Cleanup code for Day 05

--- src/bin/day05/main.rs
+++ src/bin/day05/main.rs
@@ -9,14 +9,25 @@ pub fn main() -> anyhow::Result<()> {
         .position(|r: &String| r.contains("move"))
         .unwrap();
 
-    let crate_rows = rev(&lines[..index - 2]).collect::<Vec<_>>();
+    let solution_part_one = solve_part_one(&lines, &index)?;
+    println!("Part One = {}", solution_part_one);
 
+    Ok(())
+}
+
+fn solve_part_one(lines: &Vec<String>, index: &usize) -> anyhow::Result<String> {
+    let mut crate_stacks = create_stacks(&lines, index)?;
+    let _ = parse_movements(&mut crate_stacks, &lines, index)?;
+
+    Ok(read_top_stacks(&mut crate_stacks))
+}
+
+fn create_stacks(lines: &Vec<String>, index: &usize) -> anyhow::Result<Vec<VecDeque<String>>> {
+    let crate_rows = rev(&lines[..index - 2]).collect::<Vec<_>>();
     let total_stacks = crate_rows.len() + 1;
-    let mut crate_stacks = Vec::new();
-    for _ in 0..total_stacks {
-        let empty_stack: VecDeque<String> = VecDeque::new();
-        crate_stacks.push(empty_stack);
-    }
+    let mut crate_stacks = (0..total_stacks)
+        .map(|_| VecDeque::new())
+        .collect::<Vec<VecDeque<String>>>();
 
     let separator = Regex::new(r"\[[a-zA-Z]]")?;
     for row in crate_rows {
@@ -43,8 +54,12 @@ pub fn main() -> anyhow::Result<()> {
         }
     }
 
+    Ok(crate_stacks)
+}
+
+fn parse_movements(crate_stacks: &mut Vec<VecDeque<String>>, lines: &Vec<String>, index: &usize) -> anyhow::Result<()> {
     let is_digit = Regex::new(r"\d+")?;
-    for movement_inst in &lines[index..] {
+    for movement_inst in &lines[*index..] {
         let counts = is_digit
             .find_iter(movement_inst)
             .map(|mat| mat.as_str().parse::<usize>().unwrap())
@@ -63,13 +78,18 @@ pub fn main() -> anyhow::Result<()> {
         }
     }
 
+    Ok(())
+}
+
+fn read_top_stacks(crate_stacks: &mut Vec<VecDeque<String>>) -> String {
+    let mut output = String::new();
     for index in 0..crate_stacks.len() {
         let top_of_stack = &crate_stacks[index]
             .pop_front()
             .expect("Unable to retrieve top of stack!");
 
-        print!("{}", top_of_stack)
+        output.push_str(top_of_stack.as_str());
     }
 
-    Ok(())
+    output
 }
